2b6b856d6d7f18af9722097e34772e4dc57c4bc3
Steve Meisburger <steve@dhcp-10-36-12-44.access.cit.cornell.edu>
2022-08-18 10:09:49 -0400

    modify cbf reader
    Certain cbf files were not able to be read by the cbf reader. For unknown reasons, they did not contain the full 4-character binary start sequence (only the first 3 characters matched). I removed the strict requirement for the fourth character. This might have unintended consequences?

--- +io/@PilatusHeader/read_cif_lines.m
+++ +io/@PilatusHeader/read_cif_lines.m
@@ -1,6 +1,8 @@
 function value = read_cif_lines(obj)
+%contents_pattern = ['\n--CIF-BINARY-FORMAT-SECTION--',...
+%    '(.*?)\x{0C}\x{1A}\x{04}\x{d5}'];
 contents_pattern = ['\n--CIF-BINARY-FORMAT-SECTION--',...
-    '(.*?)\x{0C}\x{1A}\x{04}\x{d5}'];
+    '(.*?)\x{0C}\x{1A}\x{04}'];
 contents_match = regexp(obj.rawheader,contents_pattern,'tokens');
 assert(~isempty(contents_match),'did not find header_contents');
 value = regexp(contents_match{1}{1},'\r\n','split');
--- +io/@PilatusHeader/seek_binary_start.m
+++ +io/@PilatusHeader/seek_binary_start.m
@@ -1,6 +1,7 @@
 function value = seek_binary_start(obj)
 nPatternBytes = 4;
-pattern = '\x{0C}\x{1A}\x{04}\x{d5}';
+%pattern = '\x{0C}\x{1A}\x{04}\x{d5}';
+pattern = '\x{0C}\x{1A}\x{04}'; % last byte sometimes missing?
 pattern_match = regexp(obj.rawheader,pattern);
 value = pattern_match + nPatternBytes - 1;
 end
